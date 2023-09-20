41588c4a950d7b02093b0b0c56eee8e3abe766ae
LoganParkhurst <parkhurst@colum.edu>
2023-09-20 11:43:28 -0500

    Currently works

---
+++ TestDungeon/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Game1;
+
+namespace TestDungeon
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //test creating a player class
+            //test creating a map class
+            //test creating a combat class
+        }
+    }
+}
